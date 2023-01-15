515c80ba4d62ab0fca87d93c85d2f1c9beab2f9d
usama7978 <122737815+usama7978@users.noreply.github.com>
2023-01-15 20:47:20 +0500

    Add files via upload

---
+++ playwithnumbers/build/lib/funnumbers/__init__.py
@@ -0,0 +1,16 @@
+def add_num(a,b):
+    return a+b
+
+def sub_num(a,b):
+    return a-b    
+
+def multi_num(a,b):
+    return a*b
+
+def divid_num(a,b):
+    return a/b
+
+def power_num(a,b):
+    return a**b   
+
+         
---
+++ playwithnumbers/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='funnumbers',
+ version='0.1',
+ author="Usama",
+ author_email="ubinkhalil11@gmail.com",
+ description="This is a simple module",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
